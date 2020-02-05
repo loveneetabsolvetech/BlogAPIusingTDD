675ca0d5133bff883e0c071e096fccc52e76fd2f
Loveneet Singh <loveneet.dotnet@outlook.com>
2020-02-05 14:20:33 +0530

    update api's for version 3.1

--- blog-using-tdd/blog-api/Startup.cs
+++ blog-using-tdd/blog-api/Startup.cs
@@ -25,7 +25,8 @@ namespace blog_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,13 @@ namespace blog_api
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            //app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
